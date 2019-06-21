a209b23690a603de25840a62d7be09b1d078c544
Mario Adamo Jr <marioadamojr@gmail.com>
2019-06-21 00:46:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post '/checkout' do
     @item = Item.new(params[:item])
     @session = session
-    @session[:item] = params[:item]
+    session[:item] = params[:item]
     erb :checkout
   end
 end
